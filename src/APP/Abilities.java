fd18489b1b09d330556885e71fe7e0cd5de4d04c
michaeldereus <michaeldereus@aol.com>
2020-04-22 04:50:11 -0500

    some attributes

--- src/APP/Abilities.java
+++ src/APP/Abilities.java
@@ -2,9 +2,6 @@ package APP;
 
 public class Abilities {
 
-    ElementGroup Physical = new ElementGroup();
-    ElementGroup Social = new ElementGroup();
-    ElementGroup Mental = new ElementGroup();
 
 
 }
--- src/APP/Attributes.java
+++ src/APP/Attributes.java
@@ -2,6 +2,19 @@ package APP;
 
 public class Attributes {
 
+    ElementGroup Physical = new ElementGroup();
+    ElementGroup Social = new ElementGroup();
+    ElementGroup Mental = new ElementGroup();
+
+//    this.Physical.addElement();
+//    this.Physical.addElement();
+//    this.Physical.addElement();
+//    this.Social.addElement();
+//    this.Social.addElement();
+//    this.Social.addElement();
+//    this.Mental.addElement();
+//    this.Mental.addElement();
+//    this.Mental.addElement();
 
 
 }
