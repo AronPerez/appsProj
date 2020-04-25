cd3d4abcd589b837b5ad8756d55b8e3e5056cc9c
AronPerez <aperez0295@gmail.com>
2020-04-25 01:44:06 -0500

    Abilities are added with object referenceS

--- src/APP/Abilities.java
+++ src/APP/Abilities.java
@@ -2,6 +2,44 @@ package APP;
 
 public class Abilities {
 
-
+    ElementGroup Talents = new ElementGroup();
+    ElementGroup Skill = new ElementGroup();
+    ElementGroup Knowledges = new ElementGroup();
+    // 11 talents
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    this.Talents.addElement();
+    // 11 skills
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    this.Skill.addElement();
+    // 11 knowledges
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
+    this.Knowledges.addElement();
 
 }
