45790609f6612630efede13fbe7cb921bb5bef44
michaeldereus <michaeldereus@aol.com>
2020-04-26 14:09:16 -0500

    shed abilities

--- src/APP/Abilities.java
+++ src/APP/Abilities.java
@@ -1,5 +1,48 @@
 package APP;
 
 public class Abilities {
-    
+
+    Element Alertness, Art, Athletics, Awareness, Brawl, Empathy, Expression, Intimidation,
+            Leadership, Streetwise, Subterfuge, Crafts, Drive, Etiquette, Firearms,
+            MartialArts, Meditation, Melee, Research, Stealth, Survival, Technology,
+            Aacademics, Computer, Cosmology, Enigma, Esoterica, Investigation, Law,
+            Medicine, Occult, Politics, Science;
+
+    public Abilities() {
+
+        Drive = new Element(0);
+        Etiquette = new Element(0);
+        Firearms = new Element(0);
+        MartialArts = new Element(0);
+        Meditation = new Element(0);
+        Melee = new Element(0);
+        Research = new Element(0);
+        Stealth = new Element(0);
+        Survival = new Element(0);
+        Technology = new Element(0);
+        Aacademics = new Element(0);
+        Computer = new Element(0);
+        Cosmology = new Element(0);
+        Enigma = new Element(0);
+        Esoterica = new Element(0);
+        Investigation = new Element(0);
+        Law = new Element(0);
+        Medicine = new Element(0);
+        Occult = new Element(0);
+        Politics = new Element(0);
+        Science = new Element(0);
+        Crafts = new Element(0);
+        Alertness = new Element(0);
+        Art = new Element(0);
+        Athletics = new Element(0);
+        Awareness = new Element(0);
+        Brawl = new Element(0);
+        Empathy = new Element(0);
+        Expression = new Element(0);
+        Intimidation = new Element(0);
+        Leadership = new Element(0);
+        Streetwise = new Element(0);
+        Subterfuge = new Element(0);
+
+    }
 }
