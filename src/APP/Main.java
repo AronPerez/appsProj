8d90a0ae1e2c0184ffe76b28f6354f9ded1f4e5b
michaeldereus <michaeldereus@aol.com>
2020-04-30 22:18:14 -0500

    manually unmerged javafx package due to build complications. please use branch service if you want to add an existing package

--- src/APP/Main.java
+++ src/APP/Main.java
@@ -1,18 +1,19 @@
 package APP;
 
-import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.application.Application;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("MageSheetMaker_view.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("View/MageSheetMaker_view.fxml"));
             Scene scene = new Scene(root,400,400);
             primaryStage.setScene(scene);
             primaryStage.show();
