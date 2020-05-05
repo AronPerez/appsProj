98d602a8792ddee4f1040d2fb215f9cafe715e93
AronPerez <aperez0295@gmail.com>
2020-05-05 16:19:46 -0500

    For pull

--- src/APP/Main.java
+++ src/APP/Main.java
@@ -12,13 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
-<<<<<<< HEAD
             Parent root = FXMLLoader.load(getClass().getResource("view/MageSheetMaker_view.fxml"));
             Scene scene = new Scene(root,400,400);
-=======
-            Parent root = FXMLLoader.load(getClass().getResource("View/MageSheetMaker_view.fxml"));
-            Scene scene = new Scene(root,800,800);
->>>>>>> 9a15d62a797195ddc3a8087bc79b07247b1b6523
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch(Exception e) {
